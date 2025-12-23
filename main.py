a7b5868ca993c8229157b20719962f8d0969c734
kgjhgdgjhk <bhrwrrhsyn@gmail.com>
2025-12-23 18:59:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,25 +3,22 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
-def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+@app.api_route("/", methods=["GET", "POST"], response_class=HTMLResponse)
+async def bmi(request: Request):
+    bmi_value = None
 
-@app.post("/calculate", response_class=HTMLResponse)
-async def calculate(request: Request):
-    form = await request.form()
-    weight = float(form["weight"])
-    height = float(form["height"]) / 100
-
-    bmi = weight / (height ** 2)
+    if request.method == "POST":
+        form = await request.form()
+        weight = float(form["weight"])
+        height = float(form["height"]) / 100
+        bmi_value = round(weight / (height ** 2), 2)
 
     return templates.TemplateResponse(
         "index.html",
         {
             "request": request,
-            "bmi": round(bmi, 2)
+            "bmi": bmi_value
         }
     )
